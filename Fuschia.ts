e0fc9db06fdf600c00062a0940beef10e1774173
thijsrijkers <thijsrijkers@live.nl>
2020-12-16 22:10:10 +0100

    🔨 Added comments

--- Fuschia.ts
+++ Fuschia.ts
@@ -2,6 +2,9 @@ class farray
 {
     value = new Array();
 
+    /**
+     * Adds any value to the farray
+     */
     Add(addValue)
     {
         this.value[this.value.length] = addValue;
@@ -13,18 +16,27 @@ class fstring
     value: string;
     constructor(value: string){this.value = value;}
 
+    /**
+     * Removes the first part and the last part of the fstring value
+     */
     StringRemovalLastFirst() 
     {
         var result = this.value.substring(1, this.value.length-1);
         return result;
     }
 
+    /**
+     * Adds a new part to the fstring value
+     */
     AddTo(addValue : string)
     {
         var result = this.value + addValue;
         return result;
     }
 
+    /**
+     * Removes specific part of fstring
+     */
     RemovePart(remove : string)
     {
         var result = this.value.replace(remove, "");
