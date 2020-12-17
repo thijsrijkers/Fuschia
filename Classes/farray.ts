9bf40e1187398b4592f9f66d362cc27ae266a30c
thijsrijkers <thijsrijkers@live.nl>
2020-12-17 15:54:55 +0100

    🎨 Created files for each class. Also added multiple functions for each class

---
+++ Classes/farray.ts
@@ -0,0 +1,75 @@
+/**
+* Head class of the fArray
+*/
+
+class farray
+{
+    value = new Array();
+
+    /**
+     * Adds any value to the farray
+     */
+    Add(addValue)
+    {
+        this.value[this.value.length] = addValue;
+    }
+
+    /**
+     * Removes any value to the farray based on the value added
+     */
+    RemoveValue(removeValue)
+    {
+        const index = this.value.indexOf(removeValue);
+        if (index > -1) 
+        {
+            this.value.splice(index, 1);
+        }
+    }
+    /**
+     * Removes first value
+     */
+    RemoveFirst()
+    {
+        this.value.shift();
+    }
+
+    /**
+     * Find value in farray and returns it
+     */
+    Find(value)
+    {
+        var result = this.value.find(value);
+        return result;
+    }
+
+    /**
+     * Find value in farray and returns it
+     */
+    Contains(value)
+    {
+        var result = false;
+
+        if(this.value.includes(value))
+        {
+            result = true;
+        }
+        return result;
+    }
+    
+    /**
+     * Removes last element and returns it
+     */
+    Pop()
+    {
+        var result = this.value.pop();
+        return result;
+    }
+
+    /**
+     * Sorts elements inside the farray
+     */
+    Sort()
+    {
+        this.value.sort();
+    }
+}
---
+++ Classes/fstring.ts
@@ -0,0 +1,56 @@
+
+class fstring 
+{
+    value: string;
+    constructor(value: string){this.value = value;}
+
+    /**
+     * Removes the first part and the last part of the fstring value
+     */
+    StringRemovalLastFirst() 
+    {
+        var result = this.value.substring(1, this.value.length-1);
+        return result;
+    }
+
+    /**
+     * Adds a new part to the fstring value
+     */
+    AddTo(addValue : string)
+    {
+        var result = this.value + addValue;
+        return result;
+    }
+
+    /**
+     * Checks if fstring contains character
+     */
+    Contains(value : string)
+    {
+        var result = false;
+
+        if(this.value.includes(value))
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
+    /**
+     * Removes specific part of fstring
+     */
+    RemovePart(remove : string)
+    {
+        var result = this.value.replace(remove, "");
+        return result;
+    }
+
+    /**
+     * Returns fstring
+     */
+    Value()
+    {
+        return this.value;
+    }
+}
